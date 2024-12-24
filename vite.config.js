8198018b9057f6cc9a06b108ca88cb736c6b5142
sunnyangee <whain100@gmail.com>
2024-12-25 05:14:07 +0900

    d

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ export default defineConfig({
     open: true, // 브라우저 자동 열기
     base: './',
   },
+  build: {
+    outDir: 'dist', // Netlify가 기본적으로 사용하는 디렉터리
+    assetsDir: '', // 정적 파일 폴더
+},
 });
